9dbc00eca38e9877524b99f96f7153900738cba3
Ronak <jainronak73@gmail.com>
2017-05-25 16:58:36 +0530

    Option to update attendance

--- app/src/main/java/com/example/admin/routineapp/stat.java
+++ app/src/main/java/com/example/admin/routineapp/stat.java
@@ -1,9 +1,17 @@
 package com.example.admin.routineapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.IntegerRes;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
@@ -12,18 +20,19 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class stat extends AppCompatActivity {
 
+    double count;
+    int[] attendance = new int[20];
+    double attended;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stat);
         GraphView graph;
         int i;
-        int size =20;
+        int size =100;
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
         graph = (GraphView) findViewById(R.id.graphs1);
-        double count,attended;
         double percentage,req;
-       int[] attendance = new int[20];
         Intent in = getIntent();
         Bundle extra = in.getExtras();
         count = extra.getInt("count");
@@ -32,28 +41,94 @@ public class stat extends AppCompatActivity {
         percentage = Math.round((percentage)*100.0)/100.0;
         req = (0.75 * count - attended)/0.25;
        attendance = extra.getIntArray("attendance");
-        int x ,y;
-        for(i = 0; i < 20; i++) {
+        Spinner spin = (Spinner)findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence> adapt = ArrayAdapter.createFromResource(this,R.array.bool,android.R.layout.simple_spinner_item);
+        adapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(adapt);
+        double x ,y;
+        x = 0;
+        y = attendance[0];
+        series.appendData(new DataPoint(x,y),true,size);
+        for(i = 1; i < count; i++) {
             x = i;
-            y = attendance[i];
-
+            y = ((double)(attendance[i] + (i)*y))/(i+1);
+            y = Math.round(y*10.0)/10.0;
             series.appendData(new DataPoint(x,y),true,size);
         }
         graph.addSeries(series);
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(20);
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinY(0);
+        graph.getViewport().setMaxY(1);
+        graph.getGridLabelRenderer().setNumHorizontalLabels(21);
+        graph.getGridLabelRenderer().setNumVerticalLabels(5);
         TextView text = (TextView)findViewById(R.id.statText);
         String s ="";
         if(req < 0){
             s = "The total number of class is :<b><h3>" + (int)count + "</h3></b>"
                     +"Out of which you have attended <b><h3>" + (int)attended + "</h3></b>" +
                     "And have a total percentage :<b><h3>" + percentage + "%</h3></b>" +
-                    "You can miss the next <h3>" + (int)Math.ceil(req) + "</h3> classes and yet maintain a 75% attendance";
+                    "You can miss the next <h3>" + -(int)Math.ceil(req) + "</h3> classes and yet maintain a 75% attendance";
         }
         else{
            s = "The total number of class is :<b><h3>" + (int)count + "</h3></b>"
                    +"Out of which you have attended <b><h3>" + (int)attended + "</h3></b>" +
                    "And have a total percentage :<b><h3>" + percentage + "%</h3></b>" +
-                    "You need to attend the next <h3>" + (int)Math.ceil(req) + "</h3> classes to maintain a 75% attendance";
+                    "You need to attend the next <h3>" + (int)Math.ceil(req) + "</h3> classes to attain a 75% attendance";
         }
         text.setText(Html.fromHtml(s));
+    }
+
+    public void submit(View view){
+        AlertDialog alertDialog = new AlertDialog.Builder(stat.this).create();
+        EditText et = (EditText)findViewById(R.id.editText3);
+        String str = et.getText().toString();
+        Spinner sp = (Spinner)findViewById(R.id.spinner2);
+        String s = sp.getSelectedItem().toString();
+        Intent in = new Intent(this,timeTable.class);
+        int cn = Integer.parseInt(str);
+        if(cn > (int)count){
+            alertDialog.setTitle("Sorry");
+            alertDialog.setMessage("Those many classes have not been held yet!");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+            et.setBackgroundColor(Color.rgb(253,237,237));
+
+        }
+        else{
+            alertDialog.setTitle("Success!");
+            alertDialog.setMessage("Your attendance has been upated!");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+            et.setBackgroundColor(Color.rgb(3,253,117));
+            if(s.compareTo("True") == 0){
+                if(attendance[cn - 1] == 0)
+                    attended = attended + 1;
+                attendance[cn - 1] = 1;
+            }
+            else {
+                if (attendance[cn - 1] == 1)
+                    attended = attended - 1;
+
+                attendance[cn - 1] = 0;
+            }
+            int an = (int)attended;
+            in.putExtra("attendance",attendance);
+            in.putExtra("attended",an);
+            setResult(1,in);
+        }
+
     }
 }
--- app/src/main/java/com/example/admin/routineapp/timeTable.java
+++ app/src/main/java/com/example/admin/routineapp/timeTable.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.TextView;
 
@@ -26,8 +27,14 @@ public class timeTable extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_table);
         int i =0;
+        cs1 = 6;
+        a1 = 3;
         for(i=0;i<20;i++){
             as1[i] = 0;
+            as2[i] = 0;
+            as3[i] = 0;
+            as4[i] = 0;
+            as5[i] = 0;
         }
         as1[1] = 0 ;
         as1[2] = 1 ;
@@ -48,20 +55,24 @@ public class timeTable extends AppCompatActivity {
     public void readFromAssets(Context context, String filename) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(context.getAssets().open(filename)));
         String mLine = reader.readLine();
-        String p,s;
+        String p,s,r,ta;
             int resId;
         while (mLine != null) {
             p = " ";
+            r ="";
+            ta = "";
             s = " ";
             p = "text" + mLine.substring(0,2);
             if(mLine.substring(0,1).equals("s")){
-                s = "S" + mLine. substring(1,2) + " : " + mLine.substring(3);
+                r = "a" +mLine.substring(1,2);
+                ta = "cs" + mLine.substring(1,2);
+                s = "S" + mLine. substring(1,2) + " : " + mLine.substring(3) + "<br>Attendance : " + r + " / " + ta;
             }else {
                 s = mLine.substring(3, 5);
             }
             resId = getResources().getIdentifier(p,"id",getPackageName());
            TextView t= (TextView)findViewById(resId);
-           t.setText(s);
+           t.setText(Html.fromHtml(s));
             mLine = reader.readLine();
         }
         reader.close();
@@ -69,16 +80,49 @@ public class timeTable extends AppCompatActivity {
 
 
     public void buttons1(View view){
-        cs1 = 6;
-        a1 = 3;
         Intent in = new Intent(this, stat.class);
-        in.putExtra("count",cs1);
-        in.putExtra("attended",a1);
-        in.putExtra("attendance",as1);
-        startActivity(in);
+        switch(view.getId()){
+            case R.id.bs1:
+                in.putExtra("count",cs1);
+                in.putExtra("attended",a1);
+                in.putExtra("attendance",as1);
 
+                break;
+            case R.id.bs2:
+                in.putExtra("count",cs2);
+                in.putExtra("attended",a2);
+                in.putExtra("attendance",as2);
 
+                break;
+            case R.id.bs3:
+                in.putExtra("count",cs3);
+                in.putExtra("attended",a3);
+                in.putExtra("attendance",as3);
 
+                break;
+            case R.id.bs4:
+                in.putExtra("count",cs4);
+                in.putExtra("attended",a4);
+                in.putExtra("attendance",as4);
 
+                break;
+            case R.id.bs5:
+                in.putExtra("count",cs5);
+                in.putExtra("attended",a5);
+                in.putExtra("attendance",as5);
+
+                break;
+        }
+        startActivityForResult(in,1);
     }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bundle extra = data.getExtras();
+        as1 = data.getIntArrayExtra("attendance");
+        a1 = extra.getInt("attended");
+    }
+
+
+
 }
